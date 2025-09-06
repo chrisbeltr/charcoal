35876efc2c60cb6afd8b00a4b1d4b759482f8244
borks <christianbeltran@uchicago.edu>
2025-09-06 11:46:07 -0500

    improved init and added cleanup function

--- termctl.h
+++ termctl.h
@@ -34,6 +34,8 @@ void get_window_size(int *cols, int *rows) {
 // storage for current terminal attributes
 termios *terminal_attrs = new termios;
 
+termios *initial_attrs = new termios;
+
 // get current terminal attributes
 #define get_terminal_attrs() tcgetattr(0, terminal_attrs);
 
@@ -51,8 +53,35 @@ termios *terminal_attrs = new termios;
 // toggle echo to the opposite of its current state
 #define toggle_echo() toggle_feature(ECHO);
 
-// perform initialization of necessary library components.
-// call this at the start of your program.
-void term_init() { get_terminal_attrs(); }
+
+/*
+ * perform initialization of necessary library components.
+ * call this at the start of your program. only use once.
+ */
+void term_init() {
+  get_terminal_attrs();
+  *initial_attrs = *terminal_attrs;
+  // disabling cannonical mode does the following:
+  // input can be read without pressing enter
+  // read() can be non-blocking
+  // line editing (done by the terminal) is disabled
+  disable_feature(ICANON);
+}
+
+/*
+ * perform cleanup of library components.
+ * call this whenever your program closes. only use once.
+ * 
+ * if this function call is skipped (e.g. premature exit due to crash),
+ * users may have to restart their terminal to restore normal function.
+*/
+void term_cleanup() {
+  // reset to initial attributes
+  *terminal_attrs = *initial_attrs;
+  set_terminal_attrs();
+  // destroy structs
+  delete terminal_attrs;
+  delete initial_attrs;
+}
 
 #endif // TERMCTL_H
